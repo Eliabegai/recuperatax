fc44cce35046f6a771ada4c6dadb308600eb84ce
Eliabe Gai <eliabegai@gmail.com>
2025-02-06 22:40:19 -0300

    feat: page empresas

---
+++ app/empresas/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { Search } from "lucide-react";
+import Layout from "../components/Layout/Layout"
+import EmpresaTable from "../components/Tabela/Empresas";
+import { useRouter } from "next/navigation";
+
+const Empresas = () => {
+  const router = useRouter()
+  return(
+    <Layout>
+      <div className="flex flex-row justify-between items-center w-full">
+        <h2 className="font-bold text-xl text-blue-900">Empresas</h2>
+        <div className="flex gap-4 text-sm text-zinc-200">
+          <div className="flex gap-2">
+            <button className="bg-emerald-500 py-2 px-3 rounded-md"><Search size={16} /></button>
+            <input type="text" placeholder="Pesquisar..." />
+          </div>
+          <button onClick={() => router.push("/cadastro")} className="bg-emerald-500 px-4 rounded-md">Cadastrar</button>
+        </div>
+      </div>
+      <EmpresaTable />
+    </Layout>
+  )
+}
+
+export default Empresas
