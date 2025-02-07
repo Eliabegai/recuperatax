5934a621de157f4073e8316bc1952288fc49c198
Eliabe Gai <eliabegai@gmail.com>
2025-02-06 22:31:55 -0300

    feat: tela cadastro, mostrar empresas

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -1,9 +1,21 @@
+import { Search } from "lucide-react";
 import Layout from "../components/Layout/Layout"
+import EmpresaTable from "../components/Tabela/Empresas";
 
 const Cadastro = () => {
   return(
     <Layout>
-      Cadastro
+      <div className="flex flex-row justify-between items-center w-full">
+        <h2 className="font-bold text-xl text-blue-900">Empresas</h2>
+        <div className="flex gap-4 text-sm text-zinc-200">
+          <div className="flex gap-2">
+            <button className="bg-emerald-500 py-2 px-3 rounded-md"><Search size={16} /></button>
+            <input type="text" placeholder="Pesquisar..." />
+          </div>
+          <button className="bg-emerald-500 px-4 rounded-md">Cadastrar</button>
+        </div>
+      </div>
+      <EmpresaTable />
     </Layout>
   )
 }
---
+++ app/components/Tabela/Empresas.tsx
@@ -0,0 +1,88 @@
+import { Eye, Edit, Trash } from "lucide-react";
+
+interface Empresa {
+  nome: string;
+  email: string;
+  cnpj: string;
+  estado: string;
+  contato: string;
+  cidade: string;
+}
+
+const empresas: Empresa[] = [
+  {
+    nome: "W. Wanderosch Carvel Auto Pec...",
+    email: "BARRAAUTOPECAS@GMAIL.COM",
+    cnpj: "34.057.391/0001-00",
+    estado: "RJ",
+    contato: "(22) 3081-2251",
+    cidade: "MACAE",
+  },
+  {
+    nome: "Raposo E Silva Clínica Odonto...",
+    email: "CONASSES@CONASSES.COM.BR",
+    cnpj: "24.532.576/0001-68",
+    estado: "MG",
+    contato: "(31) 3331-6275",
+    cidade: "BELO HORIZONTE",
+  },
+  {
+    nome: "Padaria E Confeitaria São Jos...",
+    email: "APADARIAJOSAOJOSE@HOTMAIL.COM",
+    cnpj: "51.085.959/0001-08",
+    estado: "SP",
+    contato: "(18) 3659-1247",
+    cidade: "BILAC",
+  },
+  {
+    nome: "Fba Administradora De Bens LT...",
+    email: "teste@teste.com.br",
+    cnpj: "10.487.508/0001-10",
+    estado: "SC",
+    contato: "(47) 3275-9100",
+    cidade: "JARAGUÁ DO SUL",
+  },
+];
+
+export default function EmpresaTable() {
+  return (
+    <div className="flex flex-1 flex-col w-full mx-auto p-2 overflow-x-auto gap-2 text-xs text-blue-900">
+      
+          {empresas.map((empresa, index) => (
+            <div key={index} className="grid grid-cols-7 w-full border-b justify-between items-center border-slate-300 rounded-lg p-4">
+              <div className="col-span-3">
+                <div  className="flex flex-col w-72">
+                  <span>Nome: <span className="font-bold">{empresa.nome}</span></span>
+                  <span>Email: <span className="font-bold">{empresa.email}</span></span>
+                </div>
+              </div>
+              <div className="col-span-2">
+                <div  className="flex flex-col w-72">
+                  <span>CNPJ: <span className="font-bold">{empresa.cnpj}</span></span>
+                  <span>Estado: <span className="font-bold">{empresa.estado}</span></span>
+                </div>
+              </div>
+              <div className="col-span-1">
+                <div  className="flex flex-col w-72">
+                  <span>Contato: <span className="font-bold">{empresa.contato}</span></span>
+                  <span>Cidade: <span className="font-bold">{empresa.cidade}</span></span>
+                </div>
+              </div>
+              <div className="col-span-1">
+                <div  className="flex justify-end gap-2 font-bold w-32">
+                  <button className="text-gray-600 hover:text-sky-600">
+                    <Eye size={14} absoluteStrokeWidth />
+                  </button>
+                  <button className="text-gray-600 hover:text-sky-600">
+                    <Edit size={14} absoluteStrokeWidth />
+                  </button>
+                  <button className="text-red-600 hover:text-sky-600">
+                    <Trash size={14} absoluteStrokeWidth  />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+    </div>
+  );
+}
