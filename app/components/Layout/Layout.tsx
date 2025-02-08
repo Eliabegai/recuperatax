dd98181c33518ff76e02fd564a93997585739e0d
Eliabe Gai <eliabegai@gmail.com>
2025-02-08 16:45:03 -0300

    feat: links nos icons

--- app/components/Layout/Layout.tsx
+++ app/components/Layout/Layout.tsx
@@ -1,7 +1,10 @@
+"use client"
 import Image from "next/image";
 import Sidebar from "../Sidebar/Sidebar";
 import Logo from "../../assets/logo-light-gray.png"
 import { Bell, BotIcon, User } from "lucide-react";
+import Link from "next/link";
+import { toast } from "sonner";
 
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -11,8 +14,8 @@ export default function Layout(props:any) {
       <header className="flex flex-row items-center justify-between p-2 bg-gray-100 border-b-2 border-green-500">
         <Image src={Logo} width={200} height={100} alt="Logo RecuperaTax" />
         <div className="flex justify-center w-20 p-1 gap-2">
-          <div className="flex justify-center items-center p-1 w-10 rounded-full border"><Bell /></div>
-          <div className="flex justify-center items-center p-1 w-10 rounded-full border"><User /></div>
+          <button onClick={() => toast.info("Sem menssagens no momento!")} className="flex justify-center items-center p-1 w-10 rounded-full border"><Bell /></button>
+          <Link href={"/login"} className="flex justify-center items-center p-1 w-10 rounded-full border"><User /></Link>
         </div>
       </header>
       <div className="flex flex-1 overflow-hidden">
