4b3d44175c88a816b80aa337860011a856781a24
Eliabe Gai <eliabegai@gmail.com>
2025-02-08 12:30:10 -0300

    feat: login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,15 @@
 "use client"
-import { Eye, EyeClosed, LockKeyhole, User } from "lucide-react"
+import { Eye, EyeClosed, Loader, LockKeyhole, User } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 import Logo from "../assets/logo-light-gray.png"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
 
 const Login = () => {
+
+  const router = useRouter()
 
   const [dataLogin, setDataLogin] = useState({
     nome: "",
@@ -13,6 +17,7 @@ const Login = () => {
     remember: false
   })
   const [viewPassword, setViewPassword] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {name, value} = e.target
@@ -21,21 +26,38 @@ const Login = () => {
     setDataLogin(updatedData)
   }
 
+  const handleLogin = (event:FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    if(dataLogin.nome === "admin" && dataLogin.senha === "admin") {
+      setLoading(true)
+      setTimeout(() => {
+        setLoading(false)
+        toast.success("Bem vindo!")
+        router.push("/")
+      }, 3000)
+    } else {
+      toast.error("Usuário ou senha inválidos!")
+    }
+  }
+
   return (
     <div className="flex flex-col w-full h-screen bg-gray-300 justify-center items-center">
-      <div className="flex flex-col w-[500px] h-[500px] justify-center items-center p-4 bg-gray-100 rounded-lg">
+      <div className="flex flex-col w-[500px] h-[500px] justify-center items-center p-4 bg-gray-100 rounded-lg shadow-xl">
         <div className="flex flex-col justify-center items-center mb-10">
           <div className="flex relative w-72 h-20">
             <Image src={Logo} alt="logo" fill />
           </div>
           <span className="text-xl text-sky-700 mt-2">Seja bem vindo!</span>
         </div>
+        <form className="flex flex-col w-full px-6 py-4" onSubmit={handleLogin}>
         <div className="flex flex-col w-full px-6 py-4">
           <span className={`flex fixed ${dataLogin.nome.length ? "-mt-7" : "-mt-3"} ml-4 text-sm items-end focus:-mt-7`}><span><User /></span>Usuário</span>
           <input
             type="text" 
             className="flex w-full border-b border-l border-sky-900 px-3 bg-gray-100 outline-none focus:ring-gray-500" 
-            autoFocus 
+            autoFocus
+            required
             placeholder="" 
             name="nome"
             value={dataLogin.nome}
@@ -49,14 +71,15 @@ const Login = () => {
             className="flex w-full border-b border-l border-sky-900 px-3 bg-gray-100 outline-none focus:ring-gray-500" 
             placeholder="" 
             name="senha"
+            required
             value={dataLogin.senha}
             onChange={handleChange}
           />
-          <button onClick={() => setViewPassword(!viewPassword)} className={`relative text-gray-600 hover:text-gray-900`}>{viewPassword ? <EyeClosed /> : <Eye />}</button>
+          <span onClick={() => setViewPassword(!viewPassword)} className={`relative text-gray-600 hover:text-gray-900`}>{viewPassword ? <EyeClosed /> : <Eye />}</span>
         </div>
         <div className="flex flex-row w-full justify-between items-center px-6">
           <span>
-            <input type="checkbox" />
+            <input name="remember" type="checkbox" onChange={handleChange} />
             <span>Lembrar de mim</span>
           </span>
           <span>
@@ -64,9 +87,14 @@ const Login = () => {
           </span>
         </div>
         <div className="mt-10 flex w-full">
-          <button className="flex w-full bg-emerald-500 px-4 py-2 rounded items-center justify-center text-gray-200">LOGIN</button>
+          <button type="submit" className={`flex w-full ${loading ? "bg-emerald-500 pointer-events-none" : "bg-emerald-500"}  px-4 py-2 rounded items-center justify-center text-gray-200`}>
+            {
+              <span className={`${loading ? "mr-2 animate-spin" : "hidden"}`}><Loader /></span>
+            } LOGIN
+          </button>
         </div>
-      </div>
+      </form>
+     </div>    
     </div>
   )
 }
