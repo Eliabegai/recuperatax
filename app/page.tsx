87864a91f4997c992f08c5ce3f34fd59afa0a818
Eliabe Gai <eliabegai@gmail.com>
2025-02-08 12:29:58 -0300

    feat: add toast

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Layout from "./components/Layout/Layout";
+import RecuperaTax from "./assets/icon.png"
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout>
-      Início
+      <div className="flex w-full h-screen justify-center items-center">
+        <Image src={RecuperaTax} alt="login" width={100} height={100} />
+      </div>
     </Layout>
   );
 }
